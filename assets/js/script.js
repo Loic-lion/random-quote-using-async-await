2e6509ec244d98504302b2b135663967a46abdb5
Loic Lion <loic.lion.contact@gmail.com>
2023-05-17 16:47:24 +0200

    ajout prediction de l'age avec l'api agify

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,14 +19,38 @@ function randomQuoteGen() {
         data.author +
         '"> <p class="container_quote">' +
         data.quote +
-        '</p> <p class="container_name">' +
+        '</p> <span class="container_name">' +
         data.author +
-        "</p>";
+        "</span>";
 
       section.appendChild(newDiv);
 
       section.removeChild(loader);
+
+      let completeName = data.author;
+      let splitName = completeName.split(" ");
+      let name = splitName[0];
+
+      fetch("https://api.agify.io/?name=" + name)
+        .then((response) => response.json())
+        .then((info) => {
+          let newP = document.createElement("p");
+          newP.className = "container_age";
+          newP.innerHTML = "Age prédit: " + info.age;
+          let spanName = document.querySelector(".container_name");
+
+          let containerDiv = document.createElement("div");
+          containerDiv.className = "container_predict";
+          containerDiv.appendChild(spanName);
+          containerDiv.appendChild(newP);
+          section.appendChild(containerDiv);
+        })
+        .catch((error) => {
+          section.removeChild(loader);
+          console.log("Il y a une erreur : ", error);
+        });
     })
+
     .catch((error) => {
       section.removeChild(loader);
       console.log("Il y a une erreur : ", error);
